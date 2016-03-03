308caaa28c586ecc33c4359bac83ffb4a335eeae
joegualdarrama <joegualdarrama@gmail.com>
2016-03-03 15:00:32 -0500

    Hourly class
    Hourly class working as intended

--- Source.cpp
+++ Source.cpp
@@ -18,6 +18,19 @@ class Hourly : Employee{
 private:
 	double wage, hours;
 public:
+	Hourly(string name, string social, double wage, double hours) : Employee(name, social), wage(wage), hours(hours){}
+	double calcEarned(double wage, double hours)
+	{
+		double overtime = hours - 40;
+		return((hours * wage) + (overtime * .5));
+	}
+	void print(){
+		Employee::print();
+		cout << "Wage:   $" << wage << endl;
+		cout << "Hours:   " << hours << endl;
+		cout << "Earned: $" << calcEarned(wage, hours) << endl;
+	}
+		
 
 };
 
@@ -26,5 +39,7 @@ int main()
 	Employee emp1("Joe", "12345");
 	emp1.print();
 
+	Hourly emp2("Bob", "12346", 15, 40);
+	emp2.print();
 	system("Pause");
 }
