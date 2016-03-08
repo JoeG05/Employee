3d82875266a8470088e50a46b211e1e1aa80903e
joegualdarrama <joegualdarrama@gmail.com>
2016-03-08 12:38:12 -0500

    print(), operator==
    Fixed print functions to be const, eliminated operator== error that
    stemmed from using vector shorthand for doesExist() and remove()

--- Source.cpp
+++ Source.cpp
@@ -12,38 +12,39 @@ private:
 	string name, social;
 public:
 	Employee(string name, string social) : name(name), social(social){}
-	void print() const
-	{
-		cout << name << " - " << social << endl;
-	}
+	virtual void print() const = 0;
+	
+	
 
 	string checkSoc() const {
 		return social;
 	}
 };
 
-bool operator==(const Employee &a, const Employee &b)
-{
-	if (a.checkSoc() == b.checkSoc())
-		return true;
-	return false;
+void Employee::print() const{
+	cout << "Name: " << name << endl;
+	cout << "Social: " << social << endl;
 }
+
+
 
 class Hourly : public Employee{
 private:
-	double wage, hours;
+	double wage, hours, earned;
 public:
 	Hourly(string name, string social, double wage, double hours) : Employee(name, social), wage(wage), hours(hours){}
-	double calcEarned(double wage, double hours)
+	void calcEarned(double wage, double hours)
 	{
-		double overtime = hours - 40;
-		return((hours * wage) + (overtime * .5));
+		double overtime = 0;
+		if (hours > 40)
+			overtime = hours - 40;
+		earned = (hours * wage) + (overtime * (wage * .5));
 	}
-	void print(){
+	void print() const{
 		Employee::print();
 		cout << "Wage:   $" << wage << endl;
-		cout << "Hours:   " << hours << endl;
-		cout << "Earned: $" << calcEarned(wage, hours) << endl;
+		cout << "Hours:  $" << hours << endl;
+		cout << "Earned: $" << earned << endl;
 	}
 		
 
@@ -51,17 +52,14 @@ public:
 
 class Salaried : public Employee{
 private:
-	double salary;
+	double salary, weekly;
 public:
 	Salaried(string name, string social, double salary) : Employee(name, social), salary(salary){}
-	double weeklySalary(double salary)
-	{
-		return salary / 52;
-	}
-	void print(){
-		Employee::print();
+	
+	void print() const {
+		Employee::print() ;
 		cout << "Salary:  $" << salary << endl;
-		cout << "Weekly:  $" << weeklySalary(salary) << endl;
+		cout << "Weekly:  $" << weekly << endl;
 	}
 
 };
@@ -69,32 +67,42 @@ public:
 class Roster{
 
 private:
-	vector<Employee> staff;
+	vector<Employee*> staff;
 public:
-	void add(Employee e){
+	void add(Employee &e){
 		if (doesExist(e))
 		{
 			cout << "Employee already exists" << endl;
 			return;
 		}
-		staff.push_back(e);
+		staff.push_back(&e);
 	}
 
-	void remove(Employee e){
-		staff.erase(std::remove(staff.begin(), staff.end(), e), staff.end());
+	void remove(Employee &e){
+		auto i = staff.begin();
+		for ( ; i != staff.end(); ++i)
+		{
+			if ((*i)->checkSoc() == e.checkSoc())
+				break;
+		}
+		staff.erase(i);
 	}
 
 	void print(){
-		for (std::vector<Employee>::iterator it = staff.begin(); it != staff.end(); ++it)
+		for (auto i = staff.begin(); i != staff.end(); ++i)
 		{
-			it->print();
+			
+			(*i)->print();
 			cout << endl;
 		}
 	}
 
-	bool doesExist(Employee e){
-		if (std::find(staff.begin(), staff.end(), e) != staff.end())
-			return true;
+	bool doesExist(Employee &e){
+		for (auto i = staff.begin(); i != staff.end(); ++i)
+		{
+			if ((*i)->checkSoc() == e.checkSoc())
+				return true;
+		}
 		return false;
 	}
 
@@ -102,25 +110,15 @@ public:
 
 int main()
 {
-	Employee emp1("Joe", "12345");
-	Employee emp5("Joe", "12345");
-	
-
-	Hourly emp2("Bob", "12346", 15, 40);
-	Salaried emp3("Chris", "12347", 52000);
+	Hourly emp1("Joe", "12345", 12.6, 50);
+	Salaried emp2("Bob", "12346", 52000);
 
 	
-	roster.add(emp1);
-	roster.remove(emp1);
 
 	roster.add(emp1);
-	roster.remove(emp1);
+	roster.add(emp2);
 
-	roster.add(emp5);
-
-	roster.add((Employee)emp2);
 	roster.print();
-	
-	
+
 	system("Pause");
 }
